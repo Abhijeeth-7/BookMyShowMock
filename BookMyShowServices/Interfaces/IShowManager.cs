8adb537ce351b009255da05e325cfaf35f68f145
Abhijeeth-7 <kanchiabhijeeth@gmail.com>
2021-06-21 22:07:40 +0530

    Added Interfaces for DbContext and Helper Classes that facilitate db operations

---
+++ BookMyShowServices/Interfaces/IShowManager.cs
@@ -0,0 +1,23 @@
+﻿using Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IShowManager
+    {
+        public Task CreateShow(int movieId, int ScreenId);
+        public Task<List<Show>> GetShows();
+        public Task<List<Seat>> GetSeatingData(int showid);
+        public Task<string> UpdateSeatingData(int Id, dynamic seatingString);
+        public Task<List<Show>> GetValidShows();
+        public Task<List<Show>> GetShowsInPLay();
+        public Task<List<Show>> GetExpiredShows();
+        public Task setShowStartTime(int movieId);
+        public Task setShowEndTime(int movieId);
+        public Task<Show> GetShow(int showId);
+    }
+}
---
+++ BookMyShowServices/Interfaces/ITicketManager.cs
@@ -0,0 +1,20 @@
+﻿using Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface ITicketManager
+    {
+        public Task GenerateTicket(Ticket Ticket);
+        public Task GenerateTickets(dynamic TicketInfo);
+        public Task<List<Movie>> GetTickets();
+        public Task<List<Ticket>> GetValidTickets();
+        public Task<List<Ticket>> GetExpiredTickets();
+        public Task<bool> ValidateTicket(int id);
+
+    }
+}
