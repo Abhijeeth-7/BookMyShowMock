a3c9e136250a4350c16d4febc4329853b3347af5
Abhijeeth-7 <kanchiabhijeeth@gmail.com>
2021-06-21 22:08:25 +0530

    Created Services to facilitate db operations

---
+++ BookMyShowServices/Repository/TheaterManager.cs
@@ -0,0 +1,35 @@
+﻿using Models.DataModels;
+using Dapper;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Repository
+{
+    public class TheaterManager : ITheaterManager
+    {
+        private IDbConnection _dbConnection;
+        public TheaterManager(IDBContext context)
+        {
+            _dbConnection = context.Connection();
+        }
+        public async Task<List<Theater>> GetTheaters()
+        {
+            string sql = "SELECT * FROM Theater";
+            var theaters = await _dbConnection.QueryAsync<Theater>(sql);
+            return theaters.ToList();
+        }
+        public async Task<Theater> GetTheater(int id)
+        {
+            string sql = "SELECT * FROM Theater where id = @id";
+            var param = new { id = id};
+            Theater theater = await _dbConnection.QueryFirstOrDefaultAsync<Theater>(sql,param);
+            Console.WriteLine(theater.TheaterName);
+            return theater;
+        }
+    }
+}
---
+++ BookMyShowServices/Repository/TicketManager.cs
@@ -0,0 +1,69 @@
+﻿using Models.DataModels;
+using Dapper;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Repository
+{
+    public class TicketManager: ITicketManager
+    {
+        private IDbConnection _dbConnection;
+        public TicketManager(IDBContext context)
+        {
+            _dbConnection = context.Connection();
+        }
+
+        public async Task GenerateTicket(Ticket ticket)
+        {
+            string sql = "INSERT INTO Ticket VALUES(@Id, @ShowId, @SeatId, @Price)";
+            var param = new { 
+                Id = ticket.Id, 
+                ShowId = ticket.ShowId, 
+                SeatId = ticket.SeatId, 
+                Price = ticket.Price 
+            };
+            await _dbConnection.ExecuteAsync(sql, param);
+        }
+        public async Task GenerateTickets(dynamic TicketInfo)
+        {
+            string sql = "SELECT Id FROM Ticket ORDER BY Id DESC";
+            var ticketId = (await _dbConnection.QueryAsync<int>(sql)).FirstOrDefault();
+
+            Ticket ticket = new Ticket();
+            ticket.Id = ticketId != null ? Convert.ToInt32(ticketId) + 1 : 1;
+            ticket.Price = TicketInfo.Price;
+            ticket.ShowId = TicketInfo.ShowId;
+            foreach(string seatId in TicketInfo.SeatIds)
+            {
+                ticket.SeatId = seatId;
+                await GenerateTicket(ticket);
+                ticket.Id += 1;
+            }
+        }
+
+        public Task<List<Ticket>> GetExpiredTickets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Movie>> GetTickets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Ticket>> GetValidTickets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ValidateTicket(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
