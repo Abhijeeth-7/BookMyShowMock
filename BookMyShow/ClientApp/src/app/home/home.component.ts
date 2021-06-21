b52ef235f82a5bd18ab08b3bc9bf5fcdef28cd25
Abhijeeth-7 <kanchiabhijeeth@gmail.com>
2021-06-21 22:31:03 +0530

    HomePage Component

--- BookMyShow/ClientApp/src/app/home/home.component.ts
+++ BookMyShow/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  public movies: any[];
+
+  constructor(private sharedService: SharedService, private route: ActivatedRoute, private router: Router) {  }
+
+  ngOnInit(): void {
+    this.sharedService.getMovies().subscribe(result => {
+      this.movies = result;
+    }, error => console.error(error));
+  }
+
+  selectMovie(movieId: number) {
+    this.router.navigate(['/MovieDetails', movieId]);
+  }
+
 }
