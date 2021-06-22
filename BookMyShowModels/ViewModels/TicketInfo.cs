34c3784668af8b13f0ac543be7fd71d681f3d6db
Abhijeeth-7 <kanchiabhijeeth@gmail.com>
2021-06-22 13:53:33 +0530

    Added View Models

---
+++ BookMyShowModels/ViewModels/TicketInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    class TicketInfo
+    {
+        public int Id { get; set; }
+        public int ShowId { get; set; }
+        public Array SeatIds { get; set; }
+        public int Price { get; set; }
+    }
+}
