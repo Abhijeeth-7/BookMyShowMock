ffc71f87f379351e7a24cf50af9ae6383afc122e
Abhijeeth-7 <kanchiabhijeeth@gmail.com>
2021-06-22 18:07:54 +0530

    Updated Services

---
+++ BookMyShowServices/Data/DapperDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Services.Data
+{
+    public class DapperDBContext : IDBContext
+    {
+        private readonly IConfiguration _config;
+
+        public DapperDBContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection()
+        {
+            var sql = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return sql;
+        }
+    }
+}
